bf3423c86c88c653c58582978751485a1b06f46c
nklsmbailey <97485576+nklsmbailey@users.noreply.github.com>
2022-03-11 15:13:42 -0600

    updated

--- app.js
+++ app.js
@@ -1,73 +1,72 @@
-// let msg2 = 'hello'
+let msg2 = 'hello'
 
-// let stringMsg = ((msg) => {
+let stringMsg = ((msg) => {
 
-//     setTimeout(() => {
-//         console.log(msg2)
-//     }, 1000)
+    setTimeout(() => {
+        console.log(msg2)
+    }, 1000)
 
-//     console.log(msg)
+    console.log(msg)
 
-// })
+})
 
-// stringMsg('hola')
-// ----------------------------------------------------------------
-// let word1 = 'first word'
-// let word2 = 'second word'
-// let word3 = 'third word'
-// let word4 = 'fourth word'
+stringMsg('hola')
 
-// let getWords = (() => {
+let word1 = 'first word'
+let word2 = 'second word'
+let word3 = 'third word'
+let word4 = 'fourth word'
 
-//     console.log(word1)
+let getWords = (() => {
 
-//     setTimeout(() => {
-//         console.log(word4)
-//     }, 3000)
+    console.log(word1)
 
-//     setTimeout(() => {
-//         console.log(word3)
-//     }, 2000)
+    setTimeout(() => {
+        console.log(word4)
+    }, 3000)
 
-//     setTimeout(() => {
-//         console.log(word2)
-//     }, 1000)
-// })
-// getWords()
-// -----------------------------------------------------------
+    setTimeout(() => {
+        console.log(word3)
+    }, 2000)
 
-// let done = (() => {
-//     console.log('Jobs finished!')
-// })
+    setTimeout(() => {
+        console.log(word2)
+    }, 1000)
+})
+getWords()
 
-// let countDown = ((num, callback) => {
+let done = (() => {
+    console.log('Jobs finished!')
+})
 
-//     setTimeout(() => {
-//         if (num > 0) {
-//             console.log(num)
-//             countDown(num - 1, callback)
-//         } else {
-//             callback();
-//         }
-//     }, 1000)
+let countDown = ((num, callback) => {
+
+    setTimeout(() => {
+        if (num > 0) {
+            console.log(num)
+            countDown(num - 1, callback)
+        } else {
+            callback();
+        }
+    }, 1000)
 
 
-// })
+})
 
-// countDown(4, done)
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+countDown(4, done)
+
 let lunchTime = true;         // toggle b/t true and false
 
 const orderMeSomeFood = (() => {
     return new Promise((resolve, reject) => {
         if (lunchTime === true) {
             let myLunch = {
-                lunch: 'Stroganoff',
+                lunch: 'Hummus',
                 beverage: 'Tea'
             }
             resolve(myLunch)
         } else {
-            let err = new Error('You done messed up!')
+            let err = new Error('You messed up!')
             reject(err)
         }
     })
